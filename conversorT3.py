2d2746c29faf266eeea5f0eb5361d6e8070da900
lillofm <lillofm@gmail.com>
2019-01-13 14:58:43 +0100

    conversor definitivo

---
+++ conversorT3.py
@@ -0,0 +1,34 @@
+class termometro():
+    def __init__(self):
+        self.__unidadM = "C"#los hacemos privados
+        self.__temperatura = 0
+        
+    def __conversor(self, temperatura, unidad):  #convertimos esta funcion en privada
+        if unidad == "C":
+            return "{}ºF".format(temperatura * 9/5 + 32)
+        elif unidad == "F":
+            return "{}ºC".format((temperatura -32) * 5/9)
+        else:
+            return "unidad incorrecta"
+        
+    def __str__(self):
+        return "{}º{}". format(self.__temperatura, self.__unidadM)
+    
+    def unidadMedida(self, uniM):
+        if uniM == None:
+            return self.__unidadM
+        else:
+            if self.uniM == "F" or uniM == "C":
+                self.__unidadM = uniM
+                
+    def temp (self, temperatura=None):
+        if temperatura == None:
+            return self.__teperatura
+        else:
+            self.__temperatura = temperatura
+            
+    def mide(self, uniM=None):
+        if uniM == None or uniM == self.unidadM:
+            return self.__str__()
+        else:
+            return self.__conversor(self.__temperatura, self.__unidadM)
