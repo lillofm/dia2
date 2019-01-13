930a91ff46834770d7fad7978eaf5e8afc3ce16e
lillofm <lillofm@gmail.com>
2019-01-13 13:23:11 +0100

    Herancia y sobreescritura

--- perro.py
+++ perro.py
@@ -13,3 +13,27 @@ class Perro():
     def __str__(self):
         print "Soy el perro{}".format(self.nombre)
         
+class PerroAsistencia(Perro):
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self,nombre, edad, peso)
+        self.amo = amo
+        self.__trabajando = False#lo hemos hecho privado poniendo elguion en trabajando
+        
+    def __str__(self):
+        return "Perro deasistencia de {}".format(self.amo)
+    
+    def pasear(self):
+        print("{} ayudo a mi dueño, {} a pasear".format(self.nombre, self.amo))
+        
+    def ladrar(self):
+        if self.__trabajando:#aqui tambien lo tengo que acabiar por que no puedo llamar a trabajando
+            print("shhh, no puedo ladrar")
+        else:
+            Perro.ladrar(self)
+    
+    def trabajando(self, valor =None):  #hacemos trabajando privado y creamos esta función
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
+        
